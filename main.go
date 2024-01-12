d83a0f15ad59cc5fb1a7160553bc5b5876af3610
Arkadiusz4 <arekmika4@gmail.com>
2024-01-12 22:28:38 +0100

    Update help function

--- main.go
+++ main.go
@@ -26,7 +26,18 @@ func main() {
 }
 
 func printHelp() {
-
+	fmt.Println("File encryption")
+	fmt.Println("Simple file encrypter for your day-to-day needs")
+	fmt.Println("")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	fmt.Println("\t go run . encrypt /path/to/your/file")
+	fmt.Println("")
+	fmt.Println("Commands:")
+	fmt.Println("\t encrypt \t Encrypts a file given a password")
+	fmt.Println("\t decrypt \t Tries to decrypt a file using a password")
+	fmt.Println("\t help \t\t Displays help text")
+	fmt.Println("")
 }
 
 func encryptHandle() {}
@@ -34,3 +45,7 @@ func encryptHandle() {}
 func decryptHandle() {
 
 }
+
+func getPassword()      {}
+func validatePassword() {}
+func validateFile()     {}
