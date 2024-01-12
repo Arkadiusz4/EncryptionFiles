31189483193aeb27f120c74710997abe40a6183c
Arkadiusz4 <arekmika4@gmail.com>
2024-01-12 23:42:03 +0100

    Create encrypt function

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/Arkadiusz4/EncryptionFiles
 go 1.21.3
 
 require (
-	golang.org/x/sys v0.16.0 // indirect
-	golang.org/x/term v0.16.0 // indirect
+	golang.org/x/crypto v0.18.0
+	golang.org/x/term v0.16.0
 )
+
+require golang.org/x/sys v0.16.0 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"github.com/Arkadiusz4/EncryptionFiles/filecrypt"
 	"golang.org/x/term"
 	"os"
 )
@@ -54,10 +55,27 @@ func encryptHandle() {
 	}
 
 	password := getPassword()
+	fmt.Println("Encrypting...")
+	filecrypt.Encrypt(file, password)
+	fmt.Println("File successfully protected")
 }
 
 func decryptHandle() {
+	if len(os.Args) < 3 {
+		fmt.Println("Missing the path to the file. For more info, run go run . help")
+		os.Exit(0)
+	}
 
+	file := os.Args[2]
+	if !validateFile(file) {
+		panic("File not found")
+	}
+
+	fmt.Print("Enter a password: ")
+	password, _ := term.ReadPassword(0)
+	fmt.Println("Decrypting...")
+	filecrypt.Decrypt(file, password)
+	fmt.Println("File successfully decrypted.")
 }
 
 func getPassword() []byte {
